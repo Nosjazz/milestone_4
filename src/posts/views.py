0f5a027359d4efda97cf810d1dbfad31aef087dd
Nosjazz <vandendriessche.n@hotmail.be>
2021-03-10 12:02:42 +0000

    add pagination logic in Blog

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from marketing.models import Signup
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     featured = Post.objects.filter(featured=True)
@@ -19,9 +20,21 @@ def index(request):
     return render( request, 'index.html', context)
 
 def blog(request):
-    post_list = Post.objects.all
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 4)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+
     context = {
-        'post_list': post_list
+        'queryset': paginated_queryset,
+        'page_request_var': page_request_var,
+
     }
     return render( request, 'blog.html', context)
 
