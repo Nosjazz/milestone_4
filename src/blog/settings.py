75e9c90e91a4a46b6a201736d0a21d7587424b7e
Nosjazz <vandendriessche.n@hotmail.be>
2021-04-18 23:41:06 +0000

    add secret key in environ

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k*b$$!%hiswnhp2pad)6(u4x_g)r5w%@&zlv&@2hrz4#f3+#57'
+SECRET_KEY = os,environement.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '134.209.224.211', 'https://git.heroku.com/nosjazz.git']
 
