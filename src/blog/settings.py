1f0a676fb291cd16803c025cc8256446b1e6d55b
Nosjazz <vandendriessche.n@hotmail.be>
2021-04-19 02:25:57 +0000

    add cache control in settings

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -188,6 +188,13 @@ EMAIL_USE_SSL = False
 
 #AWS Config
 if 'USE_AWS' in os.environ:
+
+    #cache 
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'nosjazz'
     AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
