6e7836b62831c629449b69770e8c3937c80157a7
Nosjazz <vandendriessche.n@hotmail.be>
2021-04-23 03:15:20 +0000

    fix views.py for donation

--- src/posts/views.py
+++ src/posts/views.py
@@ -18,8 +18,22 @@ def success(request):
 	return render(request, 'success.html', {})
 
 def donation(request):
+    if request.method == 'POST':
+        amount = int(request.POST['amount'])
 
-    
+        customer = stripe.Customer.create(
+			email=request.POST['email'],
+			name=request.POST['nickname'],
+			source=request.POST['stripeToken']
+			)
+
+        charge = stripe.Charge.create(
+			customer=customer,
+			amount=amount*100,
+			currency='usd',
+			description="Donation"
+			)
+
     return render(request, "donation.html", {})
 
 
